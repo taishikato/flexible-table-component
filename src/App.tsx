1ed85c7575f32c5c78b38a8e41d464e571ca583d
taishikato <taishi.k0903@gmail.com>
2022-07-06 16:01:49 -0500

    flexible sticky functionality

--- src/App.tsx
+++ src/App.tsx
@@ -94,127 +94,93 @@ const tableDataRaw = [
   },
 ];
 
-const isHeaderFixed = true;
-const isFirstColFixed = true;
+const isHeaderSticky = true;
+const isFirstColSticky = true;
 
 function App() {
   const [tableData, setTableData] = useState(tableDataRaw);
 
   return (
-    <>
-      {/* <div className="min-w-[600px]">
-        <div
-          className={`flex border-b border-[#0B1424] w-full ${
-            isHeaderFixed ? "fixed top-0 right-0 left-0 bg-white" : ""
-          }`}
-          style={{ gridTemplateColumns: "300px repeat(5, minmax(50px, 1fr))" }}
-        >
-          <div
-            className={`p-2 font-semibold w-[300px] ${
-              isFirstColFixed ? "fixed left-0 w-full" : ""
+    <table className="table-fixed border-[#696969] min-w-[600px] border-separate border-spacing-0">
+      <thead>
+        <tr>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white text-left font-semibold ${
+              isHeaderSticky && isFirstColSticky
+                ? "sticky top-0 z-20 left-0"
+                : ""
+            } ${
+              isHeaderSticky && !isFirstColSticky ? "sticky top-0 z-20" : ""
+            } ${
+              !isHeaderSticky && isFirstColSticky ? "sticky left-0 z-20" : ""
             }`}
           >
             Last Update: June 14th, 10:00 AM
-          </div>
-          <div
-            className={`flex-1 p-2 grid place-items-center ${
-              isFirstColFixed ? "ml-[300px]" : ""
+          </th>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white font-light ${
+              isHeaderSticky ? "sticky top-0 z-10" : ""
             }`}
           >
             Jun 12th
-          </div>
-          <div className="flex-1 p-2 grid place-items-center">Jun 13th</div>
-          <div className="flex-1 p-2 grid place-items-center">Current</div>
-          <div className="flex-1 p-2 grid place-items-center">Jun 15th</div>
-          <div className="flex-1 p-2 grid place-items-center">Jun 16th</div>
-        </div>
-        {isHeaderFixed && (
-          <div
-            draggable
-            className="grid"
-            style={{
-              gridTemplateColumns: "300px repeat(5, minmax(50px, 1fr))",
-            }}
+          </th>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white font-light ${
+              isHeaderSticky ? "sticky top-0 z-10" : ""
+            }`}
           >
-            <div className="p-2 border-b border-r border-[#E2E2E2] flex items-center gap-x-3">
-              <img src={drag} />
-              dummy
-            </div>
-          </div>
-        )}
+            Jun 13th
+          </th>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white font-light ${
+              isHeaderSticky ? "sticky top-0 z-10" : ""
+            }`}
+          >
+            Current
+          </th>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white font-light ${
+              isHeaderSticky ? "sticky top-0 z-10" : ""
+            }`}
+          >
+            Jun 15th
+          </th>
+          <th
+            className={`border-b border-[#0B1424] p-2 bg-white font-light ${
+              isHeaderSticky ? "sticky top-0 z-10" : ""
+            }`}
+          >
+            Jun 16th
+          </th>
+        </tr>
+      </thead>
+      <tbody>
         {tableData.map(({ id, name, values }, index) => (
-          <div key={id} draggable className="flex bg-white">
-            <div
-              className={`p-2 border-b border-r border-[#E2E2E2] flex items-center gap-x-3 w-[300px] ${
-                isFirstColFixed ? "fixed left-0" : ""
+          <tr key={id} draggable className="bg-white">
+            <th
+              className={`border p-2 bg-white text-left font-light ${
+                isFirstColSticky ? "sticky z-10 left-0" : ""
               }`}
             >
-              <img src={drag} />
-              {name}
-            </div>
+              <div className="flex items-center gap-x-3">
+                <img src={drag} />
+                {name}
+              </div>
+            </th>
             {values.map((value, index) => (
-              <div
+              <td
                 key={index}
-                className={`p-2 border-b border-r border-[#E2E2E2] flex-1 grid place-items-center ${
+                className={`border p-2 font-semibold ${
                   index === 2 ? "bg-[#e4f6fa]" : ""
-                } ${index > 2 ? "bg-[#f3f3f3]" : ""} ${
-                  isFirstColFixed && index === 0 ? "ml-[300px]" : ""
-                }`}
+                } ${index > 2 ? "bg-[#f3f3f3]" : ""}`}
               >
                 {value}
-              </div>
+              </td>
             ))}
-          </div>
-        ))}
-      </div> */}
-
-      <table className="table-fixed border-[#696969] min-w-[600px] border-separate border-spacing-0">
-        <thead>
-          <tr className="">
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 bg-white z-20 left-0 text-left font-semibold">
-              Last Update: June 14th, 10:00 AM
-            </th>
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 z-10 bg-white font-light">
-              Jun 12th
-            </th>
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 z-10 bg-white font-light">
-              Jun 13th
-            </th>
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 z-10 bg-white font-light">
-              Current
-            </th>
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 z-10 bg-white font-light">
-              Jun 15th
-            </th>
-            <th className="border-b border-[#0B1424] p-2 sticky top-0 z-10 bg-white font-light">
-              Jun 16th
-            </th>
           </tr>
-        </thead>
-        <tbody>
-          {tableData.map(({ id, name, values }, index) => (
-            <tr key={id} draggable className="bg-white">
-              <th className="border p-2 sticky z-10 bg-white left-0 text-left font-light">
-                <div className="flex items-center gap-x-3">
-                  <img src={drag} />
-                  {name}
-                </div>
-              </th>
-              {values.map((value, index) => (
-                <td
-                  key={index}
-                  className={`border p-2 font-semibold ${
-                    index === 2 ? "bg-[#e4f6fa]" : ""
-                  } ${index > 2 ? "bg-[#f3f3f3]" : ""}`}
-                >
-                  {value}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
