364aeb659b013021f91afca2dbd5949bcdbde8e1
taishikato <taishi.k0903@gmail.com>
2022-07-28 13:12:56 -0700

    fix link text

--- src/simpler.tsx
+++ src/simpler.tsx
@@ -5,7 +5,7 @@ const Simpler = () => {
   return (
     <div style={{ minWidth: "400px", maxWidth: "600px", margin: "0 auto" }}>
       <div style={{ margin: "15px 0" }}>
-        <Link to="/">Go to the simpler table</Link>
+        <Link to="/">Go to the old table</Link>
       </div>
       <Table />
     </div>
